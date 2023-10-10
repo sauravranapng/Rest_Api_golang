fcec41cc4d54c35aa1cc57df6e671c771109b7f6
SAURAV <82589079+sauravranapng@users.noreply.github.com>
2023-10-10 23:51:35 +0530

    Update main.go

--- main.go
+++ main.go
@@ -13,12 +13,16 @@ func main() {
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-
+         //This line initializes a Gin router using the default settings
+	//The r variable now holds an instance of the Gin router with default configurations, including middleware.
 	r.GET("ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "pong")
 	})
-
+//defines an anonymous function (closure) that takes a single parameter c, 
+//which is a context object of type *gin.Context.
+//The context object contains information about the incoming HTTP request and provides methods for handling and responding to the request.
 	userRepo := controllers.New()
+	//It calls the New function of a controllers package (or type) to create this instance.
 	r.POST("books", userRepo.CreateUser)
 	r.GET("books", userRepo.GetUsers)
 	r.GET("books/:id", userRepo.GetUser)
