e74d21cbab733dc9e675fc2b35fe8725b4eab7e9
SAURAV <82589079+sauravranapng@users.noreply.github.com>
2023-10-11 08:35:54 +0530

    Update user.go

--- controllers/user.go
+++ controllers/user.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"errors"
-	
+	//for HTTP status codes
 	"net/http"
 	"github.com/sauravranapng/modules/database"
 	"github.com/sauravranapng/modules/models"
@@ -13,22 +13,42 @@ import (
 )
 
 type books_db struct {
+
 	Db *gorm.DB
+//Db is become the database connection object from the GORM library.
 }
 
 func New() *books_db {
+
 	db := database.InitDb()
+// This line initializes a database connection using the InitDb function from the database package
+//The db variable now holds a reference to the database connection.
 	db.AutoMigrate(&models.Book{})
+//This line performs an automatic migration of the database schema.
+// It ensures that the database schema matches the Go struct representation of a book, 
+//which is typically defined in the models package as models.Book.
 	return &books_db{Db: db}
+//creates a new books_db controller instance and sets its Db field to the previously initialized database connection (db).
 }
 
 //create user
 func (Books_db *books_db) CreateUser(c *gin.Context) {
+//(Books_db *books_db): This part is the receiver, which specifies the type on which the function is defined.
+//func RECEIVER_NAME.FUNCTION_NAME(PARAMETERS) RETURN_TYPE {}
+//(c *gin.Context) :This part is the function's parameter list. It specifies the input parameters that the function expects.
+
 	var book models.Book
+
 	c.BindJSON(&book)
+//c.BindJSON(&book) is used to bind or parse the JSON data from the HTTP request body
+// and populate the book variable with it.
+//This is a common pattern in web applications to extract data sent by clients in JSON format 
+//and map it to a Go struct for further processing
 	err := models.CreateUser(Books_db.Db, &book)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
+//If an error is detected, it responds to the client with an HTTP status code 500 (Internal Server Error)
+// and includes a JSON response containing an error message
 		return
 	}
 	c.JSON(http.StatusOK, book)
