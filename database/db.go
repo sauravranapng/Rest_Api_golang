b2fe9b8bde2776008d94db728ac088d7337117e0
SAURAV <82589079+sauravranapng@users.noreply.github.com>
2023-10-13 00:31:08 +0530

    Update db.go

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ const DB_HOST = "127.0.0.1"
 const DB_PORT = "3306"
 
 var Db *gorm.DB
-
+//declaring an instance of the GORM database
 func InitDb() *gorm.DB {
 	Db = connectDB()
 	return Db
@@ -23,9 +23,12 @@ func InitDb() *gorm.DB {
 func connectDB() *gorm.DB {
 	var err error
 	dsn := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?" + "parseTime=true&loc=Local"
+        // DSN  Data Source Name (DSN) string
 	fmt.Println("dsn : ", dsn)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+         //mysql.Open(dsn) is become possible because of mysql driver 
+        //&gorm.Config{} is struct that is used to specify various configuration options 
+        // for the behavior of GORM when interacting with the database.we are passing with default setting 
 	if err != nil {
 		//fmt.Println("Error connecting to database : error=%v", err)
 		return nil
